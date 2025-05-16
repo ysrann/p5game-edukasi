6f5318b49a00e3ad36672f050f26fe6d453b6308
ysrann <faiqyusran@gmail.com>
2025-05-16 18:23:19 +0700

    Create question.js

---
+++ question.js
@@ -0,0 +1,52 @@
+const questions = [
+  {
+    question: "Apa ibu kota Indonesia?",
+    options: ["Jakarta", "Bandung", "Surabaya", "Medan"],
+    answer: "Jakarta"
+  },
+  {
+    question: "Gunung tertinggi di Indonesia adalah?",
+    options: ["Kerinci", "Semeru", "Rinjani", "Puncak Jaya"],
+    answer: "Puncak Jaya"
+  },
+  {
+    question: "Pulau terbesar di Indonesia adalah?",
+    options: ["Sumatra", "Kalimantan", "Sulawesi", "Papua"],
+    answer: "Kalimantan"
+  },
+  {
+    question: "Siapa presiden pertama Indonesia?",
+    options: ["Soeharto", "Habibie", "Sukarno", "Jokowi"],
+    answer: "Sukarno"
+  },
+  {
+    question: "Bahasa resmi negara Indonesia adalah?",
+    options: ["Jawa", "Sunda", "Bahasa Indonesia", "Melayu"],
+    answer: "Bahasa Indonesia"
+  },
+  {
+    question: "Lambang negara Indonesia adalah?",
+    options: ["Singa", "Elang", "Garuda", "Rajawali"],
+    answer: "Garuda"
+  },
+  {
+    question: "Hari kemerdekaan Indonesia diperingati pada tanggal?",
+    options: ["17 Juli", "17 Agustus", "10 November", "1 Juni"],
+    answer: "17 Agustus"
+  },
+  {
+    question: "Mata uang Indonesia adalah?",
+    options: ["Dollar", "Rupiah", "Ringgit", "Baht"],
+    answer: "Rupiah"
+  },
+  {
+    question: "Candi Borobudur terletak di provinsi?",
+    options: ["Jawa Barat", "Jawa Tengah", "Yogyakarta", "Jawa Timur"],
+    answer: "Jawa Tengah"
+  },
+  {
+    question: "Lagu kebangsaan Indonesia adalah?",
+    options: ["Indonesia Raya", "Garuda Pancasila", "Tanah Airku", "Bagimu Negeri"],
+    answer: "Indonesia Raya"
+  }
+];
